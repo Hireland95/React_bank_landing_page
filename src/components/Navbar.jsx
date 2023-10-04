afde02a3cf68505083f5b0f1e43b270981c34815
Henry Ireland <henryireland95@gmail.com>
2023-10-04 14:24:34 +0100

    working on components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import { useState } from 'react';
 import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
   <nav className="w-full flex py-6
   justify-between items-center navbar">
@@ -17,8 +18,9 @@ const Navbar = () => {
         <li
           key={nav.id}
           className={`font-poppins
-            font-normal cursor-pointer
-            text-[16-px] text-white`}
+          font-normal cursor-pointer
+          text-[16-px] ${index === navLinks.length - 1 ?
+          'mr-0' : 'mr-10'} text-white`}
         >
           <a href={`#${nav.id}`}>
              {nav.title}
@@ -26,8 +28,43 @@ const Navbar = () => {
         </li>
       ))}
     </ul>
+
+    <div className="sm:hidden flex flex-1
+    justify-end items-center">
+      <img
+        src={toggle ? close : menu}
+        alt="menu"
+        className="w-[28px] h-[28px]
+        object-contain"
+        onClick={() => setToggle((prev) => !prev)}
+      />
+
+      <div className={`${toggle ? 'flex' :
+      'hidden'} p-6 bg-black-gradient
+      absolute top-20 right-0 mx-4
+      my-2 min-w-[140px] rounded-xl
+      sidebar`}
+      >
+        <ul className="list-none flex flex-col
+        justify-end items-center flex-1">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`font-poppins
+              font-normal cursor-pointer
+              text-[16-px] ${index === navLinks.length - 1 ?
+              'mr-0' : 'mb-4'} text-white`}
+            >
+              <a href={`#${nav.id}`}>
+                {nav.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
