08e8903766e25745f4104eb0ff32798fc7ce9799
Henry Ireland <henryireland95@gmail.com>
2023-10-04 16:41:53 +0100

    finishing components

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -7,10 +7,10 @@ const Clients = () => (
     w-full`}>
       {clients.map((client) => (
         <div key={client.id} className={`flex-1 ${styles.flexCenter}
-        sm:min-w-[192px] min-w-[120px]`}>
+        sm:min-w-[192px] min-w-[120px] mb-10`}>
           <img src={client.logo} alt={client}
           className="sm:w-[192px] w-[100px]
-          object-contain" />
+          object-contain opacity-80 hover:opacity-100 cursor-pointer"/>
         </div>
       ))}
     </div>
